d2eed298dedc47a76371fc06ea704b8c2f3b2b6a
audetv <algsound@yandex.ru>
2025-01-25 18:58:12 +0300

    Добавлен функциоанл вкл/выкл режима поиска новостей под известное событие

--- geomatrix.view.ts
+++ geomatrix.view.ts
@@ -2,10 +2,10 @@ namespace $.$$ {
 	export class $terratensor_geomatrix extends $.$terratensor_geomatrix {
 
 		event_submit( ) {
-			
+
 			const result = this.$.$terratensor_geomatrix_process.auth( {
 				content: this.kml(),
-				focus_mode: true,
+				focus_mode: this.focus_mode_enebled(),
 			} )
 
 			this.download(result.file_url)
